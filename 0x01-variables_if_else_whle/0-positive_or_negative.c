d8bdf77b2c29eb731caa511fa6758f8eff72f9a0
aser <aserdeme1@gmail.com>
2022-04-10 07:19:34 -0700

    task 0

---
+++ 0x01-variables_if_else_whle/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        if (n < 0)
+
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        if (n < 0)
