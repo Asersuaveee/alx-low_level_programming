a30bc77c3ac447e89c9487967c39557af6f9bee8
aser <aserdeme1@gmail.com>
2022-04-14 13:24:30 -0700

    dss

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - entry point
+ *
+ * Description: Prints the numbers  with _putchar
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
